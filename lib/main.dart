a9cee5ffed43ea4d3546452d0503503a3edf66fe
damoklov <mishanya@protonmail.com>
2021-09-27 01:41:13 +0300

    Removed default floating button

--- lib/main.dart
+++ lib/main.dart
@@ -98,11 +98,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
